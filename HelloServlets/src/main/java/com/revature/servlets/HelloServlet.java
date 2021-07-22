28461b9f1fb6853b92e2fa5588917cce3a966df5
tjwoods03 <thomas.woods@revature.net>
2021-07-22 14:39:04 -0500

    asd

--- HelloServlets/src/main/java/com/revature/servlets/HelloServlet.java
+++ HelloServlets/src/main/java/com/revature/servlets/HelloServlet.java
@@ -18,7 +18,7 @@ public class HelloServlet extends HttpServlet{
 		
 		//Response come with a method to create a Print Writer to write the body of the response
 		PrintWriter pw = resp.getWriter();
-		pw.print("<h1>Hello from your doGet method.</h1>");
+		pw.print("<h1>Hello from your doGet method on jenkins.</h1>");
 		
 	}
 	
